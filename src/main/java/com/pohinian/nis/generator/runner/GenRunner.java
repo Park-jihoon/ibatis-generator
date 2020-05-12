2ab82a02520cf732dad3df04e1897bb0cefbf85f
박지훈 <pohinian@jitpower.com>
2020-05-12 14:01:20 +0900

    폴더를 항상 삭제하지 않도록 수정

--- src/main/java/com/pohinian/nis/generator/runner/GenRunner.java
+++ src/main/java/com/pohinian/nis/generator/runner/GenRunner.java
@@ -1,20 +1,19 @@
 package com.pohinian.nis.generator.runner;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.python.google.common.base.CaseFormat;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
@@ -30,12 +29,25 @@ public class GenRunner implements CommandLineRunner {
     @Override
     public void run(String... args) {
         List<String> tableNames = Arrays.asList(
-            "CLS_CAR_MST",
-            "CLS_CONT_CAR_DTL",
-            "CLS_CONTRACT",
-            "CLS_CONTRACT_FILE",
-            "CLS_CONTRACTOR",
-            "CLS_SUBPLC_MST"
+//            "CLL_AGREE_STORAGE",
+//            "CLL_AUTO_ACNT",
+//            "CLL_CAR_MST",
+//            "CLL_CONT_CAR_DTL",
+//            "CLL_CONT_LEA_MM",
+//            "CLL_CONT_LEA_MPAY",
+                "CLL_CONT_LEALEVY_MM",
+                "CLL_CONT_LEALEVY_MPAY",
+                "CLL_CONT_LEALEVY_MST"
+//            "CLL_CONT_LEVYEXP",
+//            "CLL_CONTRACT_FILE",
+//            "CLL_VIRTUAL_ACNT",
+//            "CLS_CAR_MST",
+//            "CLS_CONT_CAR_DTL",
+//            "CLS_CONTRACT",
+//            "CLS_CONTRACT_FILE",
+//            "CLS_CONTRACTOR",
+//            "CLS_SUBPLC_MST",
+
         );
         generateXmlFiles(tableNames);
         generateJavaFiles(tableNames);
@@ -60,10 +72,10 @@ public class GenRunner implements CommandLineRunner {
         String voStr = GenVo.makeJava(tableName, pkList, colList);
 
         Path javaDir =
-            Paths.get(defaultPath + "/java/com/jitpower/pms/nis/"
-                + tableName.substring(0, 3).toLowerCase() + "/cmm/"
-                + CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, tableName).substring(3)
-                .toLowerCase());
+                Paths.get(DEFAULT_PATH + "/java/com/jitpower/pms/nis/"
+                        + tableName.substring(0, 3).toLowerCase() + "/cmm/"
+                        + CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, tableName).substring(3)
+                        .toLowerCase());
         String daoFileName =
             CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, tableName) + "Dao.java";
         String serviceFileName =
@@ -73,10 +85,10 @@ public class GenRunner implements CommandLineRunner {
         String voFileName =
             CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, tableName) + "VO.java";
         try {
-            Files.walk(javaDir)
-                .sorted(Comparator.reverseOrder())
-                .map(Path::toFile)
-                .forEach(File::delete);
+//            Files.walk(javaDir)
+//                .sorted(Comparator.reverseOrder())
+//                .map(Path::toFile)
+//                .forEach(File::delete);
             if (!Files.isDirectory(javaDir)) {
                 javaDir = Files.createDirectories(javaDir);
             }
@@ -107,10 +119,10 @@ public class GenRunner implements CommandLineRunner {
         List<Map<String, Object>> pkList = getPkCols(tableName);
         List<Map<String, Object>> colList = getColList(tableName);
         String xmlStr = GenXml.makeXml(tableName, pkList, colList);
-        Path xmlDir = Paths.get(defaultPath + "/resources/egovframework/sqlmap/"
-            + tableName.substring(0, 3).toLowerCase() + "/cmm/");
+        Path xmlDir = Paths.get(DEFAULT_PATH + "/resources/egovframework/sqlmap/"
+                + tableName.substring(0, 3).toLowerCase() + "/cmm/");
         String fileName =
-            CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, tableName) + ".xml";
+                CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, tableName) + ".xml";
         try {
             if (!Files.isDirectory(xmlDir)) {
                 xmlDir = Files.createDirectories(xmlDir);
@@ -126,16 +138,16 @@ public class GenRunner implements CommandLineRunner {
 
     private void generateXmlFiles(List<String> tableNames) {
         String tableName = tableNames.stream().findFirst().orElse("");
-        Path xmlDir = Paths.get(defaultPath + "/resources/egovframework/sqlmap/"
-            + tableName.substring(0, 3).toLowerCase() + "/cmm/");
-        try {
-            Files.walk(xmlDir)
-                .sorted(Comparator.reverseOrder())
-                .map(Path::toFile)
-                .forEach(File::delete);
-        } catch (IOException e) {
-            log.error("에러가 발생했습니다.", e);
-        }
+        Path xmlDir = Paths.get(DEFAULT_PATH + "/resources/egovframework/sqlmap/"
+                + tableName.substring(0, 3).toLowerCase() + "/cmm/");
+//        try {
+//            Files.walk(xmlDir)
+//                .sorted(Comparator.reverseOrder())
+//                .map(Path::toFile)
+//                .forEach(File::delete);
+//        } catch (IOException e) {
+//            log.error("에러가 발생했습니다.", e);
+//        }
         tableNames.forEach(this::genXmlFile);
     }
 
